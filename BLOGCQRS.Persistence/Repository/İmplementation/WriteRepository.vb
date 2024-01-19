24848fb8598c1c619d693e0aac2ca0e20637c40d
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-01-19 17:54:12 +0300

    Changes

--- BLOGCQRS.Persistence/Repository/İmplementation/WriteRepository.vb
+++ BLOGCQRS.Persistence/Repository/İmplementation/WriteRepository.vb
@@ -7,18 +7,18 @@
     End Sub
 
     Public Sub Add(entity As T) Implements IWriteRepository(Of T).Add
-        _ctx.Set(Of T).Add(entity)
+        Task.Run(Sub() _ctx.Set(Of T).Add(entity))
     End Sub
 
     Public Sub Update(entity As T) Implements IWriteRepository(Of T).Update
-        _ctx.Set(Of T).Update(entity)
+        Task.Run(Sub() _ctx.Set(Of T).Update(entity))
     End Sub
 
     Public Sub Delete(entity As T) Implements IWriteRepository(Of T).Delete
-        _ctx.Set(Of T).Remove(entity)
+        Task.Run(Sub() _ctx.Set(Of T).Remove(entity))
     End Sub
 
     Public Sub Delete(id As Object) Implements IWriteRepository(Of T).Delete
-        _ctx.Set(Of T).Remove(id)
+        Task.Run(Sub() _ctx.Set(Of T).Remove(id))
     End Sub
 End Class
