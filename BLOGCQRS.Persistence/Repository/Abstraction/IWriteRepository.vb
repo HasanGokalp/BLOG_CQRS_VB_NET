7aaf7bc772dc3f7e457edab7206f9b55d24dd9d7
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-01-22 08:03:45 +0300

    Changes

--- BLOGCQRS.Persistence/Repository/Abstraction/IWriteRepository.vb
+++ BLOGCQRS.Persistence/Repository/Abstraction/IWriteRepository.vb
@@ -1,6 +1,6 @@
 ﻿Public Interface IWriteRepository(Of T As Class)
     Sub Add(ByVal entity As T)
-    Sub Update(ByVal entity As T)
-    Sub Delete(ByVal entity As T)
-    Sub Delete(ByVal id As Object)
+    Function Update(ByVal entity As T) As Task
+    Function Delete(ByVal entity As T) As Task
+    Function Delete(ByVal id As Object) As Task
 End Interface
--- BLOGCQRS.Persistence/Repository/İmplementation/WriteRepository.vb
+++ BLOGCQRS.Persistence/Repository/İmplementation/WriteRepository.vb
@@ -6,19 +6,19 @@
         _ctx = ctx
     End Sub
 
-    Public Sub Add(entity As T) Implements IWriteRepository(Of T).Add
-        Task.Run(Sub() _ctx.Set(Of T).Add(entity))
+    Public Async Sub Add(entity As T) Implements IWriteRepository(Of T).Add
+        Await _ctx.Set(Of T).AddAsync(entity)
     End Sub
 
-    Public Sub Update(entity As T) Implements IWriteRepository(Of T).Update
-        Task.Run(Sub() _ctx.Set(Of T).Update(entity))
-    End Sub
+    Public Async Function Update(entity As T) As Task Implements IWriteRepository(Of T).Update
+        Await Task.Run(Sub() _ctx.Set(Of T).Update(entity))
+    End Function
 
-    Public Sub Delete(entity As T) Implements IWriteRepository(Of T).Delete
-        Task.Run(Sub() _ctx.Set(Of T).Remove(entity))
-    End Sub
+    Public Async Function Delete(entity As T) As Task Implements IWriteRepository(Of T).Delete
+        Await Task.Run(Sub() _ctx.Set(Of T).Remove(entity))
+    End Function
 
-    Public Sub Delete(id As Object) Implements IWriteRepository(Of T).Delete
-        Task.Run(Sub() _ctx.Set(Of T).Remove(id))
-    End Sub
+    Public Async Function Delete(id As Object) As Task Implements IWriteRepository(Of T).Delete
+        Await Task.Run(Sub() _ctx.Set(Of T).Remove(id))
+    End Function
 End Class
